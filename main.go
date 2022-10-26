54c277e03cf7f6b9018657762d37204fea1f9e0d
InzamZ <me@inzamz.top>
2022-10-26 17:16:45 +0800

    :art: Improve the struct of code

--- main.go
+++ main.go
@@ -1,37 +1,48 @@
 package main
 
 import (
-	"flag"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 	"os"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+var TGBOTTOKEN string
+var DEBUG string
+var MASTER_ID int64
+var CHANNEL_REPORT int64
+var CHANNEL_SLEEP int64
+var err error
 
 func main() {
 
-	var TGBotToken string
-
-	//命令行参数读取
-	flag.StringVar(&TGBotToken, "tgbottoken", "", "机器人Token,必填")
-	flag.Parse()
-
-	//环境变量读取
-	if TGBotToken == "" {
-		TGBotToken = os.Getenv("TGBOTTOKEN")
+	//读取环境变量到全局变量中
+	TGBOTTOKEN = os.Getenv("TGBOTTOKEN")
+	DEBUG = os.Getenv("DEBUG")
+	MASTER_ID, err = strconv.ParseInt(os.Getenv("MASTER_ID"), 10, 64)
+	if err != nil {
+		log.Panic(err.Error())
 	}
-
-	//官方示例
-	bot, err := tgbotapi.NewBotAPI(TGBotToken)
+	CHANNEL_REPORT, err = strconv.ParseInt(os.Getenv("CHANNEL_REPORT"), 10, 64)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	CHANNEL_SLEEP, err = strconv.ParseInt(os.Getenv("CHANNEL_SLEEP"), 10, 64)
 	if err != nil {
 		log.Panic(err.Error())
 	}
 
-	if (os.Getenv("DEBUG") == "true"){
-		bot.Debug = true
-	}else {
-		bot.Debug = false
+	//使用TGBOTTOKEN登录机器人
+	bot, err := tgbotapi.NewBotAPI(TGBOTTOKEN)
+	if err != nil {
+		log.Panic(err.Error())
 	}
 	log.Default().Printf("Authorized on account %s", bot.Self.UserName)
+
+	if DEBUG == "true" {
+		bot.Debug = true
+	}
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 30
@@ -39,7 +50,10 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil && update.Message.IsCommand() { // If we got a message
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.IsCommand() { // If we got a message
 			log.Default().Printf("[COMMAND::%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			if update.Message.Command() == "start" || update.Message.Command() == "echo" {
